2d7b37ae048cc46860bbc5bc78e65846fa761372
ChiragFullstack <chiragmahajan9019@gmail.com>
2023-10-25 00:32:56 +0530

    install some package

--- routes/vehiclelog/logreport.js
+++ routes/vehiclelog/logreport.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const router=express.Router();
-//const{generateLogReport}=require('../../controller/vehicleLog');
-const{generateGraph}=require('../../controller/vehicleLog');
+const{generateLogReport}=require('../../controller/vehicleLog');
+//const{generateGraph}=require('../../controller/vehicleLog');
 //const{generateLogReportWithPdf}=require('../../controller/vehicleLog');
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.get('/generateLogReport',generateGraph);
+router.get('/generateLogReport',generateLogReport);
 module.exports=router;
