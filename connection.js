fcc61ddb2c100b44caf437924f20fa03e81b2af7
ChiragFullstack <chiragmahajan9019@gmail.com>
2024-01-14 20:20:02 +0530

    final DB connection

--- connection.js
+++ connection.js
@@ -7,26 +7,19 @@ const mysql = require('mysql2/promise');
 //     database: 'eld'
 // });
 
-    const connection = mysql.createPool({
-        host: '208.109.67.112', //162.213.251.52
-        user: 'Yadbir',//'arcopgcy_admin',   //adminEld admin edridhzx_admin
-        password:'Canteen123!@#', //'Chirag!@#123'   1OkMChGhGOKK //
-        database:'Punjabicanteen'  //edridhzx_EldBackend
-    });
-
     // const connection = mysql.createPool({
-    //     host: '162.213.251.52', //162.213.251.52
-    //     user: 'edridhzx_admin',//'arcopgcy_admin',   //adminEld admin 
-    //     password:'1OkMChGhGOKK', //'Chirag!@#123'   1OkMChGhGOKK //
-    //     database:'edridhzx_EldBackend'  //
-    // });
-    
-    // const connection = mysql.createPool({
-    //     host: '162.255.116.142', //162.213.251.52
-    //     user: 'root',//'arcopgcy_admin',   //adminEld admin edridhzx_admin
-    //     password:'CI0Lmx.5*tO)', //'Chirag!@#123'   1OkMChGhGOKK //
+    //     host: '208.109.67.112', //162.213.251.52
+    //     user: 'Yadbir',//'arcopgcy_admin',   //adminEld admin edridhzx_admin
+    //     password:'Canteen123!@#', //'Chirag!@#123'   1OkMChGhGOKK //
     //     database:'Punjabicanteen'  //edridhzx_EldBackend
     // });
+
+    const connection = mysql.createPool({
+        host: '162.255.116.142:2087', //162.213.251.52
+        user: 'edrivebook_gill',//'arcopgcy_admin',   //adminEld admin edridhzx_admin
+        password:'bOXq4NOi=*^S', //'Chirag!@#123'   1OkMChGhGOKK //
+        database:'edrivebook_punjabicanteen'  //edridhzx_EldBackend
+    });
 
 
 module.exports = connection;
